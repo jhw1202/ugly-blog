0a31c69d61bca457932f8a302dbe02692d258722
DBC Apprentice <apprentice@devbootcamp.com>
2013-03-17 17:22:54 -0700

    added editpost and delete post functionality, user authentication seems to be workzzzz yoloswag

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   # Remember to create a migration!
     before_create :assign_password
-  validates :email, :format => {:with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/, :message => "Email is not valid"}
+  validates :email, :format => {:with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/, :message => "Not Valid"}
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :hashed_password, :presence => true
